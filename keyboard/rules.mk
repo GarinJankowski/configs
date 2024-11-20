ab2d628c81c78abeeb653f5ca5261b1837ae5b29
Garin Jankowski <garin.jankowski@yahoo.com>
2024-11-19 22:05:08 -0800

    updated keymap stuff

---
+++ keyboard/rules.mk
@@ -0,0 +1,13 @@
+CAPS_WORD_ENABLE = yes
+COMBO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+OS_DETECTION_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+
+CUSTOM_MATRIX = lite
+WS2812_DRIVER = vendor
+RGBLIGHT_ENABLE = yes
+DEFERRED_EXEC_ENABLE = yes
+SRC += encoder.c
+SRC += ghosting.c
+SRC += matrix.c
